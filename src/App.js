eb6e4a6add72f9e51fddea6a7bb01aaf1fb57b19
Rakib <phpzarakib@gmail.com>
2022-03-10 19:20:03 +0600

    Start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import Article from "./components/Article/Article";
+
 function App() {
   return (
     <div className="App">
-     <h1>UX UI Page </h1>
-        
+     <h1>UX UI Page </h1> 
+     <Article/>
     </div>
   );
 }
